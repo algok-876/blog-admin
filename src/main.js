8b78b12ad0baa929c4bafc98196b20edf036dc6a
Wangwei <2219444825@qq.com>
2022-02-24 21:08:50 +0800

    feat:抽离组件导入

--- src/main.js
+++ src/main.js
@@ -1,68 +1,9 @@
 import { createApp } from "vue";
 import store from "./stores";
-import { useUserStore } from "./stores/user";
 import App from "./App.vue";
 import router from "./router";
-import {
-  create,
-  NButton,
-  NLayout,
-  NLayoutSider,
-  NLayoutContent,
-  NLayoutHeader,
-  NMenu,
-  NIcon,
-  NAvatar,
-  NGradientText,
-  NDropdown,
-  NForm,
-  NFormItem,
-  NInput,
-  NMessageProvider,
-  NSelect,
-  NGrid,
-  NGridItem,
-  NDataTable,
-  NDialogProvider,
-  NModal,
-  NCheckbox,
-  NCheckboxGroup,
-  NPagination,
-  NSpace,
-  NDatePicker,
-  NNumberAnimation
-} from "naive-ui";
+import naive from "./naive-ui"
 
-const naive = create({
-  components: [
-    NButton,
-    NLayout,
-    NLayoutSider,
-    NLayoutContent,
-    NLayoutHeader,
-    NMenu,
-    NIcon,
-    NAvatar,
-    NGradientText,
-    NDropdown,
-    NForm,
-    NFormItem,
-    NInput,
-    NMessageProvider,
-    NSelect,
-    NGrid,
-    NGridItem,
-    NDataTable,
-    NDialogProvider,
-    NModal,
-    NCheckbox,
-    NCheckboxGroup,
-    NPagination,
-    NSpace,
-    NDatePicker,
-    NNumberAnimation
-  ],
-});
 const app = createApp(App);
 app.use(router);
 app.use(store);
